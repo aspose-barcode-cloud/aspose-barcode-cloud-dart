21918b15020fb876b049f762334983c954f3034e
Denis Averin <denis.averin@aspose.com>
2024-04-23 23:36:50 +0700

    Version 1.24.4

--- lib/src/api_client.dart
+++ lib/src/api_client.dart
@@ -9,7 +9,7 @@ import 'api_helper.dart';
 import 'auth/authentication.dart';
 
 /// Current SDK Version
-const SDK_VERSION = "1.24.3";
+const SDK_VERSION = "1.24.4";
 
 /// ApiClient is responsible for making HTTP requests to the API.
 class ApiClient {
