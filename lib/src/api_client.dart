9573366e46e260331f6935d175d3366e53cab9e4
ivankamkin <109517020+ivankamkin@users.noreply.github.com>
2024-08-27 12:00:58 +0500

    Package version updated (#40)

--- lib/src/api_client.dart
+++ lib/src/api_client.dart
@@ -10,7 +10,7 @@ import 'api_helper.dart';
 import 'auth/authentication.dart';
 
 /// Current SDK Version
-const SDK_VERSION = "1.24.7";
+const SDK_VERSION = "1.24.8";
 
 /// ApiClient is responsible for making HTTP requests to the API.
 class ApiClient {
